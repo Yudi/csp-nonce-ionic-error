248beaa6c0d5a136a9a253c4018fc84316fe25d6
Yudi <12040397+Yudi@users.noreply.github.com>
2024-05-25 01:24:14 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'csp-nonce-ionic-error',
+  webDir: 'www'
+};
+
+export default config;
