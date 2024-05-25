62fa98017b3b7248655742d6c98c9471f29e332f
Yudi <12040397+Yudi@users.noreply.github.com>
2024-05-25 01:31:41 -0300

    Add CSP

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,26 @@
-import { enableProdMode } from '@angular/core';
+import { CSP_NONCE, enableProdMode } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter } from '@angular/router';
-import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
+import {
+  IonicRouteStrategy,
+  provideIonicAngular,
+} from '@ionic/angular/standalone';
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 import { environment } from './environments/environment';
+import { setNonce } from '@ionic/core/loader';
 
 if (environment.production) {
   enableProdMode();
 }
 
+const nonce = 'randomNonceGoesHere';
+setNonce(nonce);
+
 bootstrapApplication(AppComponent, {
   providers: [
+    { provide: CSP_NONCE, useValue: nonce },
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
     provideRouter(routes),
